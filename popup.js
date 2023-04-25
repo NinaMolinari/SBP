3102321b51fc50fa4dcb6981695afefd77f0aeb9
Nina Molinari <ninamolinari@yahoo.com>
2023-04-24 20:32:47 -0700

    some mods

--- popup.js
+++ popup.js
@@ -2,7 +2,7 @@
 // FILE:      popup.js
 // Copyright: Nina Molinari 2023 (c)
 //
-// PROJECT:   Browser Plugin
+// PROJECT:   Smart Browser Plugin
 //            This is my small attempt to keep the "tabs" on the 
 //            time I spend working on differenct projects.
 //            There are a few good plugins out there that manage 
@@ -21,11 +21,29 @@ function fmtDate (date) {
 
 document.addEventListener('DOMContentLoaded', function() {
   //
-    var eBtnShowHist = document.getElementById('btnShowHist');
-    var eBtnSaveHist = document.getElementById('btnSaveHist');
-    var eBtnTabsInfo = document.getElementById('btnListTabs');
-    var tableBody    = document.getElementById('historyTable');    
-    var _web_history = [];
+    var eBtnShowHist     = document.getElementById('btnShowHist');
+    var eBtnSaveHist     = document.getElementById('btnSaveHist');
+    var eBtnTabsInfo     = document.getElementById('btnListTabs');
+    var eBtnClear        = document.getElementById('btnClear');    
+    var tableBody        = document.getElementById('historyTable');     
+    var statsDiv         = document.getElementById('tabsInfo');
+    var searchInput      = document.getElementById('searchString');
+    var elSearchButton   = document.getElementById('searchButton');    
+    var bInitSearch      = true;
+    var _web_history     = [];
+
+    // elSearchButton.innerHTML = 'Search Tabs';
+    // =================================================================    
+    // Process "ClearAll" button click
+    // =================================================================
+    function ClearAll() {
+      tableBody.innerHTML = '';
+      statsDiv.innerHTML  = '';      
+    }
+    eBtnClear &&
+    eBtnClear.addEventListener('click', function() { 
+      ClearAll();
+    });
 
     // =================================================================    
     // Process "ShowHist" button click
@@ -51,14 +69,14 @@ document.addEventListener('DOMContentLoaded', function() {
             var total_count = data.length;
             var current_count = 0;
             data.forEach(function(page) {
-              console.log(page);
+              // console.log(page);
               var tr = document.createElement('tr');
               var date = new Date(page.lastVisitTime);
               var time = date.toLocaleString();
               var time = fmtDate(date);// time.substring(
               var url = (page.url).substring(0, 80);
               current_count += 1;
-              tr.innerHTML = '<td>' + current_count + '</td><td width="500">' + time + '</td><td>' + url + '</td>';          
+              tr.innerHTML = '<td>' + current_count + '</td><td>' + time + '</td><td>' + url + '</td>';          
               tableBody.appendChild(tr);
             });
           });
@@ -109,7 +127,7 @@ document.addEventListener('DOMContentLoaded', function() {
               console.log ("Active Tab Count: " + active_tabs);
             }
             else {
-              tabs_count_string += window.tabs.length + '.';
+              tabs_count_string += window.tabs.length + ' + ';
               //console.log ("Inactive Tab Count: " + total_tabs);// windows.length);
               console.log ("Inactive Window: " + window.tabs.length); // windows.length);            
             }
@@ -124,66 +142,80 @@ document.addEventListener('DOMContentLoaded', function() {
 
         // =================================================================
         chrome.tabs.query({}, function(tabs) {
-          var tabUrls = [];
+          var tabUrls   = [];
           var tabTitles = [];
           var foundTabs = [];
+          var allTabs   = tabs;
           for (var i = 0; i < tabs.length; i++) {
-            //foundTabs.push(tabs[i]);
+            // foundTabs.push(tabs[i]);
             tabUrls.push(tabs[i].url);
             tabTitles.push(tabs[i].title);
           }
-          console.log(tabUrls);
+          // console.log(tabUrls);
 
-          var statsDiv = document.createElement('div');
-          document.body.appendChild(statsDiv);
+          // var statsDiv = document.createElement('div');
+          // document.body.appendChild(statsDiv);
+          // "tabsInfo"
 
-          statsDiv.innerHTML = (`${win_count}:${total_tabs}:${tabs_count_string} <br>`); 
+          statsDiv.innerHTML = (`Windows: ${win_count} // Tabs: ${total_tabs}:${tabs_count_string} <br>`); 
 
-          var searchInput = document.createElement('input');
-          searchInput.type = 'text';
-          document.body.appendChild(searchInput);
+          // var searchInput = document.createElement('input');
+          // searchInput.type = 'text';
+          // document.body.appendChild(searchInput);
         
-          var searchButton = document.createElement('button');
-          searchButton.innerHTML = 'Search Tabs';
-          document.body.appendChild(searchButton);
-        
+          if (0) { // now created in popup.html
+              var searchButton = document.createElement('button');
+              searchButton.innerHTML = 'Search Tabs';
+              document.body.appendChild(searchButton);
+          }
           var resultsDiv = document.createElement('div');
           document.body.appendChild(resultsDiv);
-        
+
+          // =================================================================
+          // Search within open tabs
+          // ================================================================= 
           function performSearch() {
-            var searchText = searchInput.value;
-            var matchingUrls = [];
+            var searchText    = searchInput.value;
+            var matchingUrls  = [];
+            var matchingTabs  = [];        
+            foundTabs         = [];
+
+            ClearAll();
             for (var i = 0; i < tabUrls.length; i++) {
               // Search within URL strings
               if (tabUrls[i].indexOf(searchText) !== -1) {
                 matchingUrls.push(tabUrls[i]);
-                foundTabs.push(tabs[i]);
+                matchingTabs.push(tabs[i]);
+                continue;
               }
               // Search within Tab titles
               if (tabTitles[i].indexOf(searchText) !== -1) {
                 matchingUrls.push(tabUrls[i]);
-                foundTabs.push(tabs[i]);                
+                matchingTabs.push(tabs[i]);                
               }
             }
             // console.log(matchingUrls);
 
             // Block removed: see [3]
-
-            iCount=0;
-            foundTabs.forEach( function(fTab) {
-                console.log('[' + ++iCount + ']: ' + fTab.title);
-                //console.log(fTab);
-                // See [2], for details on fTab.(elements) and properties
-            });
+            // See [2], for details on fTab elements and properties
 
             //@here : SUN 4/23 13:08
             if (1) {
               // originally: resultsDiv.innerHTML = matchingUrls.join('<br>');
                  tCount = 0;
-                 foundTabs.forEach (function (mTab) {
+                 // var cTile = "";
+                 matchingTabs.forEach (function (mTab) {
+                    tCount +=1 ;
                     var tr = document.createElement('tr');
-                    tr.innerHTML = '<td> (' + ++tCount + ')</td><td width="600"><a href=' + mTab.url + '>[' + mTab.title + ']</a></td>';
-                    resultsDiv.appendChild(tr);
+                    // console.log( " original: " + tCount ":" +  mTab.title );
+                    // console.log( ` original: ${tCount} :  ${mTab.title}` );                    
+                    cTitle = mTab.title.replace(/</g,"&lt;");
+                    //string = string.replace(/</g, "&lt;").replace(/>/g, "&gt;");
+                    // console.log( ` replaced: ${tCount} :  ${cTitle}` );
+                    // string.replace(/="[^"]+"/g,function($0){return $0.replace(/&lt;/g,"<").replace(/&gt;/g,">");})
+                    tr.innerHTML = '<td> (' + tCount + ')</td><td width="600"><a href=' + mTab.url + '>[' + cTitle + ']</a></td>';
+                    //resultsDiv.appendChild(tr);
+                    statsDiv.appendChild(tr);
                  });
             }
           } // end of function performSearch()
@@ -195,7 +227,7 @@ document.addEventListener('DOMContentLoaded', function() {
             }
           });
         
-          searchButton.addEventListener('click', function() {
+          elSearchButton.addEventListener('click', function() {
               performSearch();
           });
         }); // End of chrome.tabs.query() - done collecting and processing all tabs
@@ -204,7 +236,7 @@ document.addEventListener('DOMContentLoaded', function() {
 
 /*  
 // ====================== ADDENDUM =================
-[1] : Structure of "data" object
+[1] : Structure of "data" object in Browsing history
 
       // {
       //    id:             "2512"
@@ -215,7 +247,7 @@ document.addEventListener('DOMContentLoaded', function() {
       //    visitCount:     1
       // }
 
-[2] : Structure of "fTab" object
+[2] : Structure of "fTab" object for Browser window Tab
 
         favIconUrl: "https://www.cbsnews.com/fly/bundles/cbsnewscore/icons/icon.svg?v=7976be32c7718021a8332cee4eb15ac6"
         id:         842656694
@@ -241,5 +273,22 @@ document.addEventListener('DOMContentLoaded', function() {
           tr.innerHTML = '<td>' + current_count + '</td><td width="800">' + time + '</td><td>' + url + '</td>';          
           tableBody.appendChild(tr);
         });
+
+[4] : html : search input string
+      <!-- 
+        <label for="name">Name (4 to 8 characters):</label>
+        <input type="text" id="searchString" name="srchStr" required minlength="4" maxlength="8" size="10">
+      -->
+
+[5] : Web links not opening from popup window
+Not allowed to load local resource: chrome://newtab/
+serviceworker.js:117 [...5ca7af4b98465ab821c5a15d75ef1152] fetching navigation request 
+   https://... 
+   FetchEvent {isTrusted: true, 
+                request: Request, 
+                clientId: '', 
+                resultingClientId: '2eb27fa1-bec5-4d19-8a42-1ee1774066e0', 
+                isReload: false, 
+                …}
 
 */
